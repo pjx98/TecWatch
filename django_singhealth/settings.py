4c7f6e0914aab7639db25c2a5d73a58af3216a6b
PJX <pjingxiang@gmail.com>
2021-03-14 00:27:32 +0800

    Login

--- django_singhealth/settings.py
+++ django_singhealth/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'accounts.apps.AccountsConfig',
+    'rest_framework',
+    'complaint_form.apps.ComplaintFormConfig',
 ]
 
 MIDDLEWARE = [
